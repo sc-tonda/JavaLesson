8367700c17adc24a687e602af3134f929bf5d31c
冨田 梨花 <r_tonda@sharecrest.com>
2025-09-01 17:53:22 +0900

    Java入門 - Lesson1 - Comments 追加:gitにcommitしました

---
+++ src/com/java/lesson/Variable.java
@@ -0,0 +1,11 @@
+package com.java.lesson;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int age = 0;    //変数ageを宣言し、0で初期化する。
+		System.out.println(age);    //ageを表示
+
+	}
+
+}
