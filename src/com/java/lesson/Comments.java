f47d64e1e70925b07dc82007022ed3938a7c3ada
冨田 梨花 <r_tonda@sharecrest.com>
2025-09-01 17:24:16 +0900

    Java入門 - Lesson1 - Comments 追加:gitにcommitしました

---
+++ src/com/java/lesson/Comments.java
@@ -0,0 +1,18 @@
+package com.java.lesson;
+
+public class Comments {
+	/**
+	 * Commentsクラス
+	 * このクラスは、コメントの書き方を学ぶためのクラスです。
+	 * @author r_tonda
+	 * @param 引数（パラメータ）についての説明
+	 * @return 戻り値についての説明
+	 * @throws 例外処理についての説明
+	 */
+	public static void main(String[] args) {
+		//これはコメントです。
+		
+		System.out.println("JavaDocを記載しました。");
+	}
+
+}
