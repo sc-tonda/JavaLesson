a5f67fde8e1b33f3fb9ba9c9f370cd8806adcc18
冨田 梨花 <r_tonda@sharecrest.com>
2025-09-01 18:02:53 +0900

    Java入門 - Lesson1 - Constant 追加:gitにcommitしました

---
+++ src/com/java/lesson/Constant.java
@@ -0,0 +1,11 @@
+package com.java.lesson;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		final int ADULT_AGE = 20;    //定数ADULT_AGEを宣言し、20で初期化。
+		System.out.println(ADULT_AGE);    //ADULT_AGEを表示
+
+	}
+
+}
